ef4233f87ba712fcf3cdf8570b7cd599e4fbb088
Nog <edrogonzales@hotmail.com>
2021-03-19 18:28:56 -0700

    Hoping I did not just commit all my node modules lol

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metdata";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            // is on by default
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
+};
+
+main().catch((err) => {
+    console.error(err);
+});
