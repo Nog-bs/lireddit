8fc1a39b8f2afdba56538bcc8584b4d39af5393f
Nog <edrogonzales@hotmail.com>
2021-04-02 17:11:15 -0700

    Created login/register form and connected client side to back end with URQL

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ import { UserResolver } from "./resolvers/user";
 import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
-import { MyContext } from "./types";
+import cors from "cors";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -22,12 +22,18 @@ const main = async () => {
 
     const RedisStore = connectRedis(session);
     const redisClient = redis.createClient();
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        })
+    );
 
     app.use(
         session({
             name: "qid",
             store: new RedisStore({
-                client: redisClient,
+                client: redisClient as any,
                 disableTouch: true,
             }),
             cookie: {
@@ -47,10 +53,13 @@ const main = async () => {
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+        context: ({ req, res }) => ({ em: orm.em, req, res }),
     });
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
 
     app.listen(4000, () => {
         console.log("------ Server Running on localhost:4000 ------");
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavBar } from "../components/NavBar";
+
+const Index = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>hello world</h1>
+        </div>
+    );
+};
+
+export default Index;
