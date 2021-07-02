830a6a73eae58f2a0b87a2086d10acb08a7961e2
nog-bs <edrogonzales@hotmail.com>
2021-07-02 16:21:32 -0700

    Cleaned upvote functionality

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { Link } from "@chakra-ui/layout";
-import { Box, Heading, Stack, Text, Flex, Button } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import React, { useState } from "react";
 import { Layout } from "../components/Layout";
+import { UpdootSection } from "../components/UpdootSection";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
@@ -30,16 +30,15 @@ const Index = () => {
                 <div>loading...</div>
             ) : (
                 <Stack spacing={8}>
-                    {data?.posts.posts.map((p) => (
-                        <>
-                            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-                                <ChevronUpIcon />
-                                <ChevronDownIcon />
+                    {data?.posts.posts.map((p, i) => (
+                        <Flex key={i} p={5} shadow="md" borderWidth="1px">
+                            <UpdootSection post={p} />
+                            <Box>
                                 <Heading fontSize="xl">{p.title}</Heading>{" "}
                                 <Text>posted by {p.creator.username}</Text>
-                                <Text mt={4}>{p.textSnippet}...</Text>
+                                <Text mt={4}>{p.textSnippet}</Text>
                             </Box>
-                        </>
+                        </Flex>
                     ))}
                 </Stack>
             )}
